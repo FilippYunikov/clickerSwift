0ad11b6c8676192bbe0b928902f9980e23042a22
@rostovHomer <filipprnd161@gmail.com>
2023-11-24 22:05:58 +0300

    обновил проект

--- clickerYaPracticum/ViewController.swift
+++ clickerYaPracticum/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    private var counter = 0
+
+    @IBOutlet weak var upButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "Значение счётчика: \(counter)"
+
     }
 
 
+    @IBAction func counterUp(_ sender: Any) {
+        counter += 1
+              counterLabel.text = "Значение счётчика: \(counter)"
+
+    }
+    
 }
 
